3af003b15fee04ceac627cd74f33ad3c13370a1e
AlejandroBacca <bacca.alejandroet36@gmail.com>
2024-03-11 12:05:28 -0300

    Shop

---
+++ shop_diagnostico/src/main/java/com/mycompany/shop_diagnostico/Shop_diagnostico.java
@@ -0,0 +1,29 @@
+
+package com.mycompany.shop_diagnostico;
+
+import Class.Producto;
+import java.util.Scanner;
+
+public class Shop_diagnostico {
+
+    public static void main(String[] args) {
+        Producto productos[] = new Producto[5]; 
+        Scanner entrada = new Scanner(System.in);
+        
+        
+        for(int i = 0; i<productos.length; i++){
+            System.out.println("ingrese nombre de producto");
+            String nombre = entrada.next();
+            System.out.println("ingrese precio de producto");
+            double precio = entrada.nextDouble();
+            System.out.println("ingrese cantidad de producto");
+            int cantidad = entrada.nextInt();
+            Producto producto = new Producto(nombre, precio, cantidad);
+            productos[i] = producto;
+        }
+         for(int i = 0; i<productos.length; i++){
+             System.err.println(productos[i]);
+        }
+    }
+    
+}
