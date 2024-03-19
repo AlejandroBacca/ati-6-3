7147d280a745fa0ed2edd7b3971802aa58099d21
AlejandroBacca <bacca.alejandroet36@gmail.com>
2024-03-19 10:42:50 -0300

    Cambios 19/3+

--- shop_diagnostico/src/main/java/com/mycompany/shop_diagnostico/Shop_diagnostico.java
+++ shop_diagnostico/src/main/java/com/mycompany/shop_diagnostico/Shop_diagnostico.java
@@ -9,20 +9,25 @@ public class Shop_diagnostico {
     public static void main(String[] args) {
         Producto productos[] = new Producto[5]; 
         Scanner entrada = new Scanner(System.in);
+        int i = 0;
+        int opc  = 1;
         
-        
-        for(int i = 0; i<productos.length; i++){
-            System.out.println("ingrese nombre de producto");
+        while(i<productos.length && opc == 1){
+            System.out.println("ingrese nombre de producto: ");
             String nombre = entrada.next();
-            System.out.println("ingrese precio de producto");
+            System.out.println("ingrese precio de producto: ");
             double precio = entrada.nextDouble();
-            System.out.println("ingrese cantidad de producto");
+            System.out.println("ingrese cantidad de producto: ");
             int cantidad = entrada.nextInt();
             Producto producto = new Producto(nombre, precio, cantidad);
             productos[i] = producto;
-        }
-         for(int i = 0; i<productos.length; i++){
-             System.out.println(productos[i]);
+            i++;
+            System.out.println("¿Desea ingresar otro producto? (1 = SI, 2 = NO): ");
+            opc = entrada.nextInt();
+         }
+        
+         for(int j = 0; j<productos.length; j++){
+             System.out.println(productos[j]);
         }
     }
     
